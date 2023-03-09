21a75809f34c698ac0183fd8aeeddf521fe07f3c
Phillip Bailey <ztron500@gmail.com>
2023-03-08 19:16:55 -0500

    Update PostForm.jsx

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -16,56 +16,56 @@ setPosts(response.data);
 fetchPosts();
 }, []);
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      const response = await axios.post('http://localhost:3001/api/posts', { title, body, author });
-      console.log(response.data);
-      setPosts([...posts, response.data]);
-      setTitle('');
-      setBody('');
-      setAuthor('');
-    } catch (error) {
-      console.error('Error creating post:', error);
-    }
-  };
+const handleSubmit = async (event) => {
+event.preventDefault();
+try {
+const response = await axios.post('http://localhost:3001/api/posts', { title, body, author });
+console.log(response.data);
+setPosts([...posts, response.data]);
+setTitle('');
+setBody('');
+setAuthor('');
+} catch (error) {
+console.error('Error creating post:', error);
+}
+};
 
-  const handleInputChange = (event) => {
-    const { id, value } = event.target;
-    if (id === 'title') setTitle(value);
-    else if (id === 'body') setBody(value);
-    else if (id === 'author') setAuthor(value);
+const handleInputChange = (event) => {
+const { id, value } = event.target;
+if (id === 'title') setTitle(value);
+else if (id === 'body') setBody(value);
+else if (id === 'author') setAuthor(value);
 
-    setDisabled(title === '' || body === '' || author === '');
-  };
+setDisabled(title === '' || body === '' || author === '');
+};
 
 
-  return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="title">Title:</label>
-        <input type="text" id="title" value={title} onChange={handleInputChange} />
-        <label htmlFor="body">Body:</label>
-        <textarea id="body" value={body} onChange={handleInputChange}></textarea>
-        <label htmlFor="author">Author:</label>
-        <input type="text" id="author" value={author} onChange={handleInputChange} />
-        <button type="submit" disabled={disabled}>Submit</button>
-      </form>
+return (
+<>
+<form onSubmit={handleSubmit}>
+<label htmlFor="title">Title:</label>
+<input type="text" id="title" value={title} onChange={handleInputChange} />
+<label htmlFor="body">Body:</label>
+<textarea id="body" value={body} onChange={handleInputChange}></textarea>
+<label htmlFor="author">Author:</label>
+<input type="text" id="author" value={author} onChange={handleInputChange} />
+<button type="submit" disabled={disabled}>Submit</button>
+</form>
 
-      <div>
-        <h2>Posts:</h2>
-        <ul>
-          {posts.map((post) => (
-            <li key={post._id}>
-              <h3>{post.title}</h3>
-              <p>{post.body}</p>
-              <p>{post.author}</p>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </>
-  );
+<div>
+<h2>Posts:</h2>
+<ul>
+{posts.map((post) => (
+<li key={post._id}>
+<h3>{post.title}</h3>
+<p>{post.body}</p>
+<p>{post.author}</p>
+</li>
+))}
+</ul>
+</div>
+</>
+);
 }
 
 export default PostForm;
